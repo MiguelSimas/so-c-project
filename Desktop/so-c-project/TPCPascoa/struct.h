31dfe7e861f42e909f9044a5d038589a8fbcbc09
MiguelSimas <miguelsimas54@hotmail.com>
2018-03-30 16:56:20 +0000

    TPC Pascoa

---
+++ Desktop/so-c-project/TPCPascoa/struct.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct alunos
+{
+    int idade;
+    char nome[30];
+    char curso[100];
+    char universidade[100];
+    int anoCurso;
+};
